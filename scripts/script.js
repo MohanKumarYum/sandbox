72d495705aa710ac973316f38e6c0f351b725415
Mohan Kumar <MohanKumarYum@Gmail.com>
2018-10-11 18:05:08 +1100

    Initial Commit - 4th Session Acadgild - [CSS Syntax]

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a')); // October 8th 2018, 5:36:17 am
+console.log(moment().format('dddd'));                    // Monday
+console.log(moment().format("MMM Do YY"));               // Oct 8th 18
+console.log(moment().format('YYYY [escaped] YYYY'));     // 2018 escaped 2018
+console.log(moment().format());  
