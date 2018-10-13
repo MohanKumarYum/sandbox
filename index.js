9f6cf26c07bd41bc7979aac17713e5f7d118ff41
Mohan Kumar <MohanKumarYum@Gmail.com>
2018-10-14 10:08:50 +1100

    Nodejs Setup PPT - FSWD-S18-Acadgild - complete.

---
+++ index.js
@@ -0,0 +1,7 @@
+import moment from 'moment';
+
+const now = moment();
+
+// use features of moment library 
+console.log(now.format('Do MMM YYYY')); // 14th Oct 2018
+console.log(now.format('ddd, h A')); // Sun, 8 AM
